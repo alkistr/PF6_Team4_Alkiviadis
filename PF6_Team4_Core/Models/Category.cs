4dde27bb66dc9981dc5ca44fdf4a1150c6c4a259
atpap <atpap@LAPTOP-5A0IHL9B>
2021-05-31 19:49:26 +0300

    Project Table Configuration

---
+++ PF6_Team4_Core/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF6_Team4_Core.Models
+{
+    public enum Category
+    {
+        Software,
+        Technology,
+        Hardware,
+        Games,
+        Music,
+        Films
+    }
+}
---
+++ PF6_Team4_Core/Models/Options/ProjectOptions/SearchProjectOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF6_Team4_Core.Models.Options.ProjectOptions
+{
+    public class SearchProjectOptions
+
+    {
+        public string SearchText{ get; set; }
+        public int CategoryId { get; set; }
+    }
+}
