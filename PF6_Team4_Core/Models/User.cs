0683ca569a526af8ff023e93877ee0cefd9c2f9f
Maria-Anna Karapanou <mkarapanou@athtech.gr>
2021-06-01 12:34:56 +0300

    User,UserService

--- PF6_Team4_Core/Models/User.cs
+++ PF6_Team4_Core/Models/User.cs
@@ -8,6 +8,11 @@ namespace PF6_Team4_Core.Models
     public class User
     {
         public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Address { get; set; }
 
     }
 }
